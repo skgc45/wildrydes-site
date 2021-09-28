86489b5fc398097b46374d898a157327e6dd61a2
skgc45 <s.sakachan@hotmail.co.jp>
2021-09-28 15:19:51 +0900

    Modify a invokeUrl

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@ window._config = {
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://easnohjdq1.execute-api.ap-northeast-1.amazonaws.com/prod'
+         // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
